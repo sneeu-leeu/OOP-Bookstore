9605f359656be03ef8ccbf227712fb84979cee6e
sneeu-leeu <sa070197@gmail.com>
2021-10-12 16:12:33 +0200

    New errors

--- person.rb
+++ person.rb
@@ -9,10 +9,11 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @legs = legs
   end
 
   def can_use_services
-    true if of_age? || @parent_permission
+    true if of_age? || @parent_permission(true)
   end
 
   def check_name
