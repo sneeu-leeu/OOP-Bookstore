9870352ff666b8f624476a30db156967ca1af50d
sneeu-leeu <sa070197@gmail.com>
2021-10-13 11:01:41 +0200

    Create relationship: student & classroom

--- classroom.rb
+++ classroom.rb
@@ -3,5 +3,11 @@ class Classroom
 
   def initialize(label)
     @label = label
+    @students = []
+  end
+
+  def add_students(student)
+    @students << student
+    student.classroom = self
   end
 end
--- student.rb
+++ student.rb
@@ -9,4 +9,9 @@ class Student < Person
   def play_hooky
     puts "¯\(ツ)/¯"
   end
+
+  def add_classroom=(classroom)
+    @classroom = classroom
+    classroom.student << self unless classroom.student.include?(self)
+  end
 end
