a263afdf36ebf0873fefc20b10ca28d5f406e14a
sneeu-leeu <sa070197@gmail.com>
2021-10-14 12:43:33 +0200

    Set person id to random generated nr

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ class Person
   attr_reader :id
 
   def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = id
+    @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
