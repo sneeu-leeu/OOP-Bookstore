d177e20a56adec8572b8ff5cba4fa7b961c5fb6f
Caleb Anderson <buyananderson@gmail.com>
2021-10-20 12:48:32 +0200

    Correct linter errors

--- app.rb
+++ app.rb
@@ -8,13 +8,13 @@ class App
   attr_accessor :people, :books, :displays, :rentals
 
   def initialize
-    @peopleOI = InOutPut.new('people.json')
-    @booksIO = InOutPut.new('books.json')
-    @rentalsIO = InOutPut.new('rentals.json')
+    @people_io = InOutPut.new('people.json')
+    @books_io = InOutPut.new('books.json')
+    @rentals_io = InOutPut.new('rentals.json')
 
-    @people = Convertor.people_h_to_ar @peopleIO.read
-    @books = Convertor.hash_to_books_arr @booksIO.read
-    @rental = Convertor.rentals_h_to_r @rentalsIO.read, @books, @people
+    @people = Convertor.people_h_to_ar @people_io.read
+    @books = Convertor.hash_to_books_arr @books_io.read
+    @rental = Convertor.rentals_h_to_r @rentals_io.read, @books, @people
     @displays = Display.new(@books, @rental, @people)
   end
 
@@ -36,13 +36,13 @@ class App
   def create_book
     new_book = Creator.book
     @books << new_book
-    @booksIO.write(new_book.to_hash)
+    @books_io.write(new_book.to_hash)
   end
 
   def create_rental
     new_rental = Creator.rental(@books, @people)
     @rental << new_rental
-    @rentalsIO.write(new_rental.to_hash)
+    @rentals_io.write(new_rental.to_hash)
   end
 
   def create_person
@@ -54,11 +54,11 @@ class App
     case person_response
     when '1'
       new_person = Creator.student
-      @peopleIO.write(new_person.to_hash)
+      @people_io.write(new_person.to_hash)
       @people << new_person
     when '2'
       new_person = Creator.teacher
-      @peopleIO.write(new_person.to_hash)
+      @people_io.write(new_person.to_hash)
       @people << new_person
     else
       puts 'Please Choose 1 - Student or 2 - Teacher'
