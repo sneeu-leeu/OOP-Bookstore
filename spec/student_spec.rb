e3da396b35619f79d260d246818697398d708fa7
Caleb Anderson <buyananderson@gmail.com>
2021-10-21 11:03:20 +0200

    Test the instance of a Student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,5 +1,17 @@
 require_relative 'required_files'
 
 describe Student do
+    before :each do 
+     @classroom = Classroom.new('A1')
+     @student = Student.new(age: 25, classroom: @classroom)
+    end 
+    describe '#instance' do
+        context 'When initialized with age and classroom' do
+             it 'should return an instance of student' do
+            expect(@student).to be_an_instance_of Student
+             end
+        end
+        
+    end
   
 end
